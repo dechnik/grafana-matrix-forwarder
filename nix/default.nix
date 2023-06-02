ac63dd35d113b87c89b9fa5332aae30c4e21fb6a
lukasz Dechnik <lukasz@dechnik.net>
2023-06-02 18:05:17 +0200

    nix basic setup + hydra

--- nix/default.nix
+++ nix/default.nix
@@ -4,7 +4,7 @@ buildGoModule rec {
   version = "0.7.0";
   proxyVendor = true;
 
-  vendorSha256 = "sha256-5P+TGUR6gSorDnsJ3xMxmu9spbH/ZeXcT563kgHFG8A=";
+  vendorSha256 = "sha256-Pgx1UwVZcxFmrFeYeE3JhlmT5ZBbKdmsSRqZrtyj5dg=";
 
   src = ../src;
 }
